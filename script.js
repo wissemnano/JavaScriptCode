9fc835bf8c92c09c68816e0e1a5b608b98386580
wissem <wissem.mansouri.ing@gmail.com>
2019-03-23 18:39:19 +0100

    Java Script file

---
+++ script.js
@@ -0,0 +1,24 @@
+
+var name = "Wissem";
+var nombre= 12;
+
+console.log(name);
+console.log(nombre);
+var nombreToStr= nombre.toString(); // Convertir en string
+console.log("to Sting "+nombreToStr);
+// Longeur d'une chaine caractere
+var lenth = name.length;
+console.log(lenth);
+// Covertir en nombre
+var xString= '12.55';
+var xNombre = parseInt(xString);
+console.log(xNombre);
+
+// convertir float
+var xFloat = parseFloat(xString);
+console.log(xFloat);
+
+// Position mot dans string
+var myString = "Hellow je suis wissem mansouri";
+var positionStr= myString.indexOf('mansouri');
+console.log("posion est := "+positionStr);
