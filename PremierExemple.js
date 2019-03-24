e0f72ef9aed926db78ba2c0d6af8d8cc0e69195a
wissem <wissem.mansouri.ing@gmail.com>
2019-03-24 10:31:48 +0100

    Manipulation des chaine des caractere

---
+++ PremierExemple.js
@@ -0,0 +1,16 @@
+// Récupere les donnes de visiteurs
+// Capturer les entres des visiteurs
+
+//var nomVisiteur = prompt("Tapez votre nom SVP");
+//alert("Bonjour "+nomVisiteur);
+
+// Convertir Chaine en majuscule
+var maChaine = "ceci est Un test des chaine !";
+var majusclke= maChaine.toUpperCase();
+console.log(majusclke);
+
+// Convertir en Minuscule
+var minuscule = maChaine.toLocaleLowerCase();
+console.log(minuscule);
+
+//
