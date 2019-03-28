98b39568297bad4111cd1fcc0279e0d99f7bc247
wissem <wissem.mansouri.ing@gmail.com>
2019-03-28 10:25:19 +0100

    fonctionalite de localStorage

--- EnvoieDonnee/js/index.js
+++ EnvoieDonnee/js/index.js
@@ -15,3 +15,29 @@ window.localStorage
 Syntaxe pour SAUVER des données sur le stockage local:
 localStorage.setItem("key", "value");
 */
+
+/*
+
+Syntaxe
+window.localStorage
+Syntaxe pour SAUVER des données sur le stockage local:
+
+localStorage.setItem("key", "value");
+Syntaxe pour LIRE les données de localStorage:
+
+var lastname = localStorage.getItem("key");
+Syntaxe pour SUPPRIMER les données de localStorage:
+
+localStorage.removeItem("key");
+
+Exemple
+L'exemple suivant compte le nombre de fois qu'un utilisateur a cliqué sur un bouton:
+
+if (localStorage.clickcount) {
+  localStorage.clickcount = Number(localStorage.clickcount) + 1;
+} else {
+  localStorage.clickcount = 1;
+}
+document.getElementById("result").innerHTML = "You have clicked the button " +
+localStorage.clickcount + " time(s).";
+*/
