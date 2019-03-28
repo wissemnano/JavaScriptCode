35a13b9c98d76b2ee10d0279ff823286d108919c
wissem <wissem.mansouri.ing@gmail.com>
2019-03-28 09:57:32 +0100

    function recieve

--- EnvoieDonnee/js/index.js
+++ EnvoieDonnee/js/index.js
@@ -1,3 +1,17 @@
 function sendTextElement() {
 	localStorage.setItem("textTransmis", document.getElementById('send').value);
 }
+
+function recieveTextElement() {
+//Syntaxe pour LIRE les données de localStorage:
+	var lastname = localStorage.getItem("key");
+
+}
+
+/* Syntaxe pour SUPPRIMER les données de localStorage:
+localStorage.removeItem("key");
+Syntaxe
+window.localStorage
+Syntaxe pour SAUVER des données sur le stockage local:
+localStorage.setItem("key", "value");
+*/
